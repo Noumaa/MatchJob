bafb91664c8a8d62515884eeeb08bf9a4435059d
Nouma <39864318+Noumaa@users.noreply.github.com>
2023-03-01 17:30:44 +0100

    Messing around with `EasyAdminBundle`

---
+++ src/Controller/Admin/OfferCrudController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Offer;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class OfferCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Offer::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+//            IdField::new('id')->hideOnForm(),
+//            TextField::new('label'),
+//            TextEditorField::new('description'),
+//            TextEditorField::new('description'),
+//            DateTimeField::new('createdAt')->hideOnForm(),
+//            TextField::new('user'),
+        ];
+    }
+}
